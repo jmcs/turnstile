a975772618a4095afde55cec507cd92b251eb838
João Santos <joao.santos@zalando.de>
2015-03-04 11:28:55 +0100

    PF-5862 make commit-msg hook dummy receive the right arguments

--- git_hooks/commit_msg.py
+++ git_hooks/commit_msg.py
@@ -3,5 +3,23 @@
 
 from __future__ import print_function
 
-if __name__ == '__main__':
+from click import command, argument
+
+
+@command()
+@argument('message_file')
+def commit_msg(message_file):
+    """
+    This hook is invoked by git commit, and can be bypassed with --no-verify option. It takes a single parameter,
+    the name of the file that holds the proposed commit log message.
+    Exiting with non-zero status causes the git commit to abort.
+
+    :param message_file: the name of the file that holds the proposed commit log message
+    :type message_file: string
+    """
+
     print('commit-msg hook')
+    print(message_file)
+
+if __name__ == '__main__':
+    commit_msg()
