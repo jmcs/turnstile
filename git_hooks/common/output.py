ac99d4d73e37d14709b082dd8400d3fb4d468730
João Santos <joao.santos@zalando.de>
2015-03-17 14:06:44 +0100

    PF-5862 Common output module

--- git_hooks/common/output.py
+++ git_hooks/common/output.py
@@ -1,1 +1,11 @@
-__author__ = 'jsantos'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+
+
+def get_logger(command, verbosity):
+    output = logging.getLogger('githooks-'+command)
+    output.setLevel(verbosity)
+    output.addHandler(logging.StreamHandler())
+    return output
