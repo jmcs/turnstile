06b0bbe1fa75a58983cc02d5ffffebbb0b889561
João Santos <joao.santos@zalando.de>
2015-03-18 11:41:47 +0100

    PF-5862 Print current value in config

--- git_hooks/manager_subcommands/config.py
+++ git_hooks/manager_subcommands/config.py
@@ -19,7 +19,7 @@ def cmd():
     # TODO move this to a question cli util
     current_verbosity = user_config.verbosity
     verbosity_levels = ['WARNING', 'INFO', 'DEBUG']
-    print('Select the git hook verbosity:')
+    print('Select the git hook verbosity (Current Value {}):'.format(current_verbosity))
     for i, level in enumerate(verbosity_levels, start=1):
         print('  {i}. {level}'.format(**locals()))
     max_option = len(verbosity_levels)
