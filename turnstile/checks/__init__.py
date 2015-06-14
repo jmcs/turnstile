236858537f1021b433a2b9e4fda486f5d5e70d15
João Santos <jmcs@jsantos.eu>
2015-06-14 15:55:35 +0200

    https://github.com/zalando/turnstile/issues/20 get checks as extensions

--- turnstile/checks/__init__.py
+++ turnstile/checks/__init__.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import collections
-import importlib
+import pkg_resources
 
 import turnstile.common.output as output
 # we need to import the following modules so import will work on python3
-import turnstile.checks.commit_msg  # noqa
-import turnstile.checks.pre_commit  # noqa
-
-
-CheckResult = collections.namedtuple('CheckResult', ['successful', 'details'])
 
 
 class CheckIgnore(Exception):
@@ -55,46 +49,21 @@ class Check(object):
         return function
 
 
-def load_check(hook_name, check_name):
+def get_checks(hook_name):
     """
-    Returns a check function.
-    Checks for hook_name are stored git_hooks.checks.[hook_name].[check_name] and should be a function name check
-
-    :param hook_name: Which hook is fetching the check
-    :param check_name: Name of check module
-    :return: check function
-    :rtype: function
-    """
-
-    logger = output.get_sub_logger(hook_name, 'load_check')
-    checks_module = 'turnstile.checks.' + hook_name.replace('-', '_')
-    sub_module_name = '.' + check_name.replace('-', '_')
-    logger.debug('Loading %s from %s', check_name, checks_module)
-    try:
-        sub_module = importlib.import_module(sub_module_name, checks_module)
-    except ImportError:
-        logger.debug("Check %s not found. Maybe it belongs to the other hook", check_name)
-        return None
-    check_function = sub_module.check
-    return check_function
-
-
-def get_checks(hook_name, checklist):
-    """
-    Load all the checks for a repository ignoring unknown checks
+    Load all the checks for the hook
 
     :param hook_name: Which hook is fetching the checks
     :type hook_name: str
     :param checklist: List of check names
     :type checklist: [str]
-    :return: [function]
+    :rtype: [str, FunctionType]
     """
-    checks_for_hook = list()
-    for check_name in checklist:
-        check = load_check(hook_name, check_name)
-        if check:
-            checks_for_hook.append(check)
-    return checks_for_hook
+    group_name = 'turnstile.{}'.format(hook_name)
+    for entry_point in pkg_resources.iter_entry_points(group_name):
+        module = entry_point.load()
+        check = module.check  # type: FunctionType
+        yield entry_point.name, check
 
 
 def run_checks(hook_name, user_configuration, repository_configuration, check_object, ):
@@ -112,11 +81,13 @@ def run_checks(hook_name, user_configuration, repository_configuration, check_ob
     :rtype: int
     """
 
-    logger = output.get_sub_logger(hook_name, 'run_checks')
+    logger = output.get_sub_logger(hook_name.replace('_', '-'), 'run_checks')
     failed_checks = 0
     checklist = repository_configuration.get('checks', [])
-    checks_to_run = get_checks(hook_name, checklist)
+    check_functions = get_checks(hook_name)
+    checks_to_run = (check for check_name, check in check_functions if check_name in checklist)
     for check in checks_to_run:
+
         try:
             result = check(user_configuration, repository_configuration, check_object)
         except CheckIgnore:
